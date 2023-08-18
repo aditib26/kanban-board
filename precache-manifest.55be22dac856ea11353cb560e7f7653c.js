81ec915b88610e38b1482e9df04636676cee549c
Aditi Bhatia <2020.aditi.bhatia@ves.ac.in>
2023-08-19 01:36:30 +0530

    Updates

---
+++ precache-manifest.55be22dac856ea11353cb560e7f7653c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "01cda2ae5305f804b539",
+    "url": "/kanban-board/static/css/main.a47c50b9.chunk.css"
+  },
+  {
+    "revision": "01cda2ae5305f804b539",
+    "url": "/kanban-board/static/js/main.01cda2ae.chunk.js"
+  },
+  {
+    "revision": "205849d973eaa941ab0f",
+    "url": "/kanban-board/static/js/1.205849d9.chunk.js"
+  },
+  {
+    "revision": "dafb7d4997a6e2b439ba",
+    "url": "/kanban-board/static/js/2.dafb7d49.chunk.js"
+  },
+  {
+    "revision": "c254250b32cde7337b09",
+    "url": "/kanban-board/static/js/runtime~main.c254250b.js"
+  },
+  {
+    "revision": "83da9e201b68c2cb273ff6596227dd15",
+    "url": "/kanban-board/index.html"
+  }
+];
